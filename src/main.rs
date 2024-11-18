21b648e10aa4f510cedabe17e97c80a6160c7ecd
Oscar Arroyo Vega <arroscar@gmail.com>
2024-11-18 18:56:17 +0100

    "initial commit"
    "Rust repo with cargo structure"

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    println!("Start training script");
+
+    // 1 load data CSV file to disk
+//    download_csv_file();
+
+}
+//    use reqwest::blocking::get(url);
+
+//    async fn download_csv_file() -> () {
+//         let url = "https://raw.githubusercontent.com/stedy/Machine-Learning-with-R-datasets/master/house_price.csv";
+//        let response = reqwest::get(url).await?;
+//        let mut file = File::create("house_price.csv").await?;
+//        copy(response.body(), &mut file).await?;
+//    }
