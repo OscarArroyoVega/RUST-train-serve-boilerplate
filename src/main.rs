326a2c97226c7907e2b2afb39bc955163c9aecdf
Oscar Arroyo Vega <arroscar@gmail.com>
2024-11-27 21:36:40 +0100

    feat(main.rs): load CSV data into DataFrame

--- src/main.rs
+++ src/main.rs
@@ -4,39 +4,52 @@
 // 4. Train a XGBoost model
 // 5. Push the model to the AWS s3 bucket (model registry)
 
+use polars::prelude::*;
+
 fn main() -> anyhow::Result<()> {
     println!("Start training script");
 
     // 1 load external CSV file to disk
     let csv_file_path = download_csv_file()?;
 
+    // 2 load the CSV file into memory
+    let df = load_csv_file(&csv_file_path)?;
+
     Ok(())
 }
 
-/// use reqwest::blocking::get(url); 
-/// block means that the function will wait for the request to complete before returning a value.
-/// blocking is used for syncronous code.
-/// anyhow is a crate that provides a Result type for error handling.
-/// :: is used to call a method on an object.
-/// ? is used to return an error.
-/// -> is used to specify the return type of the function.
-/// <()> is a generic type that represents a tuple with no elements.
-/// -> anyhow::Result<()> is a generic type that represents a Result type with no elements.
 
 fn download_csv_file() -> anyhow::Result<(String)> {
-        // to declare a variable we use let
-        let url: &str = "https://raw.githubusercontent.com/stedy/Machine-Learning-with-R-datasets/master/house_price.csv";
-        // return error instead   
-        let response = reqwest::blocking::get(url)?;
-        // get the csv and save it to in memory data type.
-        let bytes = response.bytes()?;
-        //copy this bytes to a disk
 
-        let file_path = "boston_housing.csv";   
+    // to declare a variable we use let
+    let url: &str = "https://raw.githubusercontent.com/selva86/datasets/master/BostonHousing.csv";  
+    
+    // get the csv and save it to in memory data type.
+    let response = reqwest::blocking::get(url)?;
 
-        // Copy the bytes to a file on disk
-        std::fs::write(file_path, bytes)?;  
+    // get the csv and save it to in memory data type.
+    let bytes = response.bytes()?;
+    //copy this bytes to a disk
 
-        Ok(file_path.to_string())
+    // create a file path
+    let file_path = "boston_housing.csv";   
 
-   }
+    // Copy the bytes to a file on disk
+    std::fs::write(file_path, bytes)?;  
+
+    Ok(file_path.to_string())
+}
+
+
+
+fn load_csv_file(file_path: &str) -> anyhow::Result<DataFrame> {
+
+    // load the CSV file from disk into Polars DataFrame
+    let df: DataFrame = CsvReader::new(std::fs::File::open(file_path)?)
+    .finish()?;
+
+    // print the first 5 rows of the DataFrame
+    println!("{:?}", df.head(Some(5)));
+
+    Ok(df)
+}
